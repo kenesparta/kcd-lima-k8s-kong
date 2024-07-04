14444ec4db7b86208c1ad00156f889a014b93ee5
Ken Esparta <kenesparta@pm.me>
2024-07-03 21:15:59 -0500

    alter directories to give more organized

---
+++ k8s/kong-service/kong.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+kubectl create ns kong
+helm install kong kong/kong -f kong-conf.yaml \
+    --set proxy.type=NodePort,proxy.http.nodePort=30000,proxy.tls.nodePort=30003 \
+    --set ingressController.installCRDs=false \
+    --set serviceMonitor.enabled=true \
+    --set serviceMonitor.labels.release=promstack \
+    --namespace kong
--- k8s/kong/kind.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-kind create cluster --name kong-kcd --config clusterconfig.yaml
-kubectl cluster-info --context kind-kong-kcd
